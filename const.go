0d21939465de2f0b335988fbf5351626743c7bf6
Duc Nhat Anh Tran <olegtran2006@gmail.com>
2025-02-20 01:08:21 +0300

    что -то сделали

---
+++ const.go
@@ -0,0 +1,30 @@
+package main
+
+/*const (
+	EmojiStart        = "\U000025B6"
+	EmojiEnd          = "\U000025C0"
+	EmojiBrain        = "\U0001F9E0"
+	EmojiEye          = "\U0001F441"
+	EmojiManShrugging = "\U0001F937"
+	EmojiSpeakingHead = "\U0001F5E3"
+
+	ButtAnswerQuestion  = EmojiEnd + "Ответить на вопрос" + EmojiStart
+	ButtAddYourQuestion = EmojiEnd + "Добавить свой вопрос" + EmojiStart
+	ButtAnswerCategory1 = EmojiBrain + "Образовательные"
+	ButtAnswerCategory2 = EmojiEye + "Психологические"
+	ButtAnswerCategory3 = EmojiManShrugging + "Другое"
+	ButtPeopleCategory1 = EmojiEnd + "По возрасту" + EmojiStart
+	ButtPeopleCategory2 = EmojiEnd + "По курсу" + EmojiStart
+	ButtPeopleCategory3 = EmojiEnd + "По полу" + EmojiStart
+	ButtPeopleCategory4 = EmojiEnd + "По факультету" + EmojiStart
+
+	ButtCodeAnswerQuestion  = "answerQuestion"
+	ButtCodeAddYourQuestion = "addQuestion"
+	ButtCodeAnswerCategory1 = "answerCategory1"
+	ButtCodeAnswerCategory2 = "answerCategory2"
+	ButtCodeAnswerCategory3 = "answerCategory3"
+	ButtCodePeopleCategory1 = "peopleCategory1"
+	ButtCodePeopleCategory2 = "peopleCategory2"
+	ButtCodePeopleCategory3 = "peopleCategory3"
+	ButtCodePeopleCategory4 = "peopleCategory4"
+)*/
