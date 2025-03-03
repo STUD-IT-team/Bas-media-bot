2958dc3be5b9afb44c8386ab12c50b1d081ad13b
Alexandr Formakovskiy <Form49.d@gmail.com>
2025-03-03 23:00:22 +0300

    Logger config implemented.

--- src/run.py
+++ src/run.py
@@ -1,3 +1,5 @@
+import os
+import sys
 import asyncio
 import redis
 import logging
@@ -12,13 +14,20 @@ from middleware.storage import StorageMiddleware
 from middleware.log import LogMiddleware
 from middleware.auth import AuthMiddleware
 
+# Logging config options
+LOGGING_KWARGS = {
+    "format": "[%(levelname)s, %(asctime)s, %(filename)s] func %(funcName)s, line %(lineno)d: %(message)s;",
+    "datefmt": "%d.%m.%Y %H:%M:%S",
+    "stream": sys.stdout,
+    "level": logging.INFO if (os.getenv("LOGGER_DEBUG", '0') != '1') else logging.DEBUG
+}
+
 
 async def main() -> None:
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-
     pgcred = PostgresCredentials(**GetPgCredEnv())
     redcred = RedisCredentials(**GetRedisCredEnv(), db=0)
     try:
@@ -31,12 +40,13 @@ if __name__ == "__main__":
     bot = Bot(token=GetBotTokenEnv(), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=RedisStorage.from_url(f"redis://{redcred.user}:{redcred.password}@{redcred.host}:{redcred.port}/1"))
     dp.include_router(UnknownRouter)
-    
-    dp.update.outer_middleware(LogMiddleware(logging.getLogger("bas-bot-logger")))
+
+    logger = logging.getLogger("bas-bot-logger")
+    logging.basicConfig(**LOGGING_KWARGS)
+
+    dp.update.outer_middleware(LogMiddleware(logger))
     dp.update.outer_middleware(StorageMiddleware(PgRedisStorage, pgcred, redcred))
     dp.update.outer_middleware(AuthMiddleware())
-    
 
 
     asyncio.run(main())
-
