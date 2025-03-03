11b06366bafe61d1fd47e7ca9a0e2eb1b158a484
impervguin <leon53000@yandex.ru>
2025-03-03 21:27:48 +0300

    Added default-state for admin and member

---
+++ src/handlers/usertransition.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from keyboards.default.admin import AdminDefaultKeyboard
+from keyboards.default.member import MemberDefaultKeyboard
+from handlers.state import MemberStates, AdminStates
+from storage.domain import Activist
+
+from aiogram.types import Message
+from aiogram.types.reply_keyboard_remove import ReplyKeyboardRemove
+from aiogram.fsm.context import FSMContext
+
+
+
+
+async def TransitToMemberDefault(message: Message, state: FSMContext, activist: Activist):
+    await state.clear()
+    await state.set_data({"user-type": "activist"})
+    await state.set_state(MemberStates.Default)
+    await message.answer(f"Активист, {activist.Name}! Что хотите сделать?", reply_markup=MemberDefaultKeyboard.Сreate())
+
+
+async def TransitToAdminDefault(message: Message, state: FSMContext, activist: Activist):
+    await state.clear()
+    await state.set_data({"user-type": "admin"})
+    await state.set_state(AdminStates.Default)
+    await message.answer(f"Админ, {activist.Name}! Что хотите сделать?", reply_markup=AdminDefaultKeyboard.Сreate())
+
+async def TransitToUnauthorized(message: Message, state: FSMContext):
+    await state.clear()
+    await message.answer("Не могу найти вас в своих базах, если это ошибка - напишите администратору", reply_markup=ReplyKeyboardRemove())
+
+
+
---
+++ src/keyboards/default/admin.py
@@ -0,0 +1,29 @@
+from aiogram.types.reply_keyboard_markup import ReplyKeyboardMarkup
+from aiogram.types.keyboard_button import KeyboardButton
+
+class AdminDefaultKeyboard:
+    AddEventButtonText = "Добавить мероприятие"
+    InfoEventButtonText = "Инфа о меро"
+    CancelEventButtonText = "Отменить мероприятие"
+    CompleteEventButtonText = "Завершить мероприятие"
+    ExportButtonText = "Экспорт меро"
+    AddMemberButtonText = "Добавить пользователя"
+    DeleteMemberButtonText = "Удалить пользователя"
+    BroadcastButtonText = "Массовая рассылка"
+
+    @staticmethod
+    def Сreate():
+        return ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton(text=__class__.AddEventButtonText)],
+                [KeyboardButton(text=__class__.InfoEventButtonText)],
+                [KeyboardButton(text=__class__.CancelEventButtonText)],
+                [KeyboardButton(text=__class__.CompleteEventButtonText)],
+                [KeyboardButton(text=__class__.AddMemberButtonText)],
+                [KeyboardButton(text=__class__.DeleteMemberButtonText)],
+                [KeyboardButton(text=__class__.BroadcastButtonText)],
+                [KeyboardButton(text=__class__.ExportButtonText)]
+            ],
+            resize_keyboard=True,
+            one_time_keyboard=False
+        )
