127c895544d4182cc4490c4f8fa31ee2678d0b6a
Alexandr Formakovskiy <Form49.d@gmail.com>
2025-05-05 00:41:10 +0300

    Report adding. Work started.

--- src/handlers/state.py
+++ src/handlers/state.py
@@ -3,7 +3,7 @@ from aiogram.fsm.state import StatesGroup, State
 class MemberStates(StatesGroup):
     Default = State()
     
-class MemberAddingReportStates(StatesGroup):
+class MemberReportAddingStates(StatesGroup):
     Choosing = State()
     ChoosingType = State()
     EnteringLink = State()
