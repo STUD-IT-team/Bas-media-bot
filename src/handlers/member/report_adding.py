a08dce2e147cc4922d5c07e1aab10aeb8d2e5222
Alexandr Formakovskiy <Form49.d@gmail.com>
2025-05-05 06:41:22 +0300

    Added handler for Member Report Adding. Added state transition.

---
+++ src/handlers/member/report_adding.py
@@ -0,0 +1,17 @@
+from aiogram.types import Message
+from aiogram import Router, F
+from handlers.state import MemberReportAddingStates
+from storage.storage import BaseStorage
+from utils.strings import NewlineJoin
+from aiogram.fsm.context import FSMContext
+from handlers.usertransition import TransitToMemberDefault
+
+from keyboards.events.activeevents import ActiveEventsKeyboard
+
+MemberReportAddingRouter = Router()
+
+
+async def TransitToMemberReportAdding(message : Message, storage : BaseStorage, state : FSMContext, logger : Logger):
+    await state.set_state(MemberReportAddingStates.ChoosingEvent)
+    activeEvents = storage.GetActiveEvents()
+    await message.answer("По какому мероприятию?", reply_markup=ActiveEventsKeyboard(activeEvents).Create())
